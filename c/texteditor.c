de40829cc4988b20e197dd28b1891f719d99a2be
Jan Hořava <czechinthemail@gmail.com>
2017-11-25 21:30:10 -0800

    added default window size

--- c/texteditor.c
+++ c/texteditor.c
@@ -7,6 +7,11 @@ static void activate(GtkApplication* app, gpointer user_data){
     GtkWidget *window;
 
     window = gtk_application_window_new(app);
+    gtk_window_set_default_size(
+      GTK_WINDOW(window),
+      800,
+      600
+    );
     gtk_window_maximize(GTK_WINDOW(window));
     gtk_window_set_title(
       GTK_WINDOW(window),
