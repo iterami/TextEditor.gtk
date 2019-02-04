ec085cf37d529982c7a5065fd50f594ef67dab19
Jan Hořava <czechinthemail@gmail.com>
2019-02-04 14:47:37 -0800

    changed g_object_unref() to g_object_ref_sink() to fix warning

--- c/main.c
+++ c/main.c
@@ -735,7 +735,7 @@ void activate(GtkApplication* app, gpointer data){
         }
 
         gtk_widget_destroy(temp_text_view);
-        g_object_unref(temp_text_view);
+        g_object_ref_sink(temp_text_view);
     }
 
     g_free(temp_path);
@@ -2677,5 +2677,5 @@ void update_opened_files(void){
     g_free(path);
 
     gtk_widget_destroy(temp_text_view);
-    g_object_unref(temp_text_view);
+    g_object_ref_sink(temp_text_view);
 }
